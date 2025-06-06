ab8c8e039abab360434ba40c9ab69aaf384e307a
btbuilds <bthompson8807@gmail.com>
2025-06-06 18:50:58 -0400

    Added updatable and drawable containers

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 # dt = delta time
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # sets player position to center of screen
+
 
     while True:
         for event in pygame.event.get():
@@ -16,8 +21,9 @@ def main():
                 return
             
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for x in drawable:
+            x.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000 # caps FPS to 60
